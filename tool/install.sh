701c09842f0a180f19e7153ad5f216ddf3c6e8dc
simpart <one_stunique@yahoo.co.jp>
2016-05-08 21:59:01 +0900

    Update install.sh

--- tool/install.sh
+++ tool/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-
 # install by (trut source root directory)/tool/install.sh
-php ../src/php/ini/install.php $*;
+SCRIPT_DIR=$(cd $(dirname $0);pwd);
+php $SCRIPT_DIR/../src/php/ini/install.php $*;
